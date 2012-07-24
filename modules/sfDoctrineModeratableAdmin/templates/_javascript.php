f718396464697869229e1e6d856d68b06dee4d69
Jo Carter <github@jocarter.co.uk>
2012-07-24 06:11:12 -0700

    Enabling use_slots for moderation js - adds moderation_js slot

--- modules/sfDoctrineModeratableAdmin/templates/_javascript.php
+++ modules/sfDoctrineModeratableAdmin/templates/_javascript.php
@@ -2,5 +2,7 @@
 // send through as partial var - or use from config
 if (empty($javascript)) $javascript = sfConfig::get('app_moderation_javascript', 'JQuery');
 
-include_partial('sfDoctrineModeratableAdmin/javascript' . $javascript);
+slot('moderation_js', get_partial('sfDoctrineModeratableAdmin/javascript' . $javascript));
+
+if (!sfConfig::get('app_moderation_use_slots', false)) include_slot('moderation_js');
 ?>
